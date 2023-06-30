b879da1ba2465a7c0a703b9da265bc1efc3ed874
Jacob Suchorabski <tauter_vigor_0k@icloud.com>
2023-06-30 19:23:10 +0200

    Improved error handling
    + Added error handling to enable logging when bot does not work
    + Added comments to the code

--- buy.js
+++ buy.js
@@ -1,85 +1,119 @@
 const axios = require('axios');
 const KrakenClient = require('kraken-api');
-const key = "INSERT_YOUR_API_KEY";
-const secret = "INSERT_YOUR_API_SECRET";
-const slack_web_hook = "INSERT_YOUR_SLACK_WEBHOOK";
-const kraken = new KrakenClient(key, secret);
 
+// Configuration
+const config = {
+  apiKey: "INSERT_YOUR_API_KEY",
+  apiSecret: "INSERT_YOUR_API_SECRET",
+  slackWebhook: "INSERT_YOUR_SLACK_WEBHOOK"
+};
+
+const kraken = new KrakenClient(config.apiKey, config.apiSecret);
+
+// Entry point
 main();
 
+// Main function
 async function main() {
+  // Define the desired amount to spend in Euros
+  const EUR_BUY_AMOUNT = 5.00;
 
-    const EUR_BUY_AMOUNT = 5.00; //change to whatever value you want
-    let price = parseFloat(await getCurrentBitcoinPrice()).toFixed(2);
-    let volume = parseFloat(EUR_BUY_AMOUNT / price).toFixed(8);
-    let balances = await kraken.api('Balance');
+  // Get the current Bitcoin price in Euros
+  let price = parseFloat(await getCurrentBitcoinPrice()).toFixed(2);
 
-    await placeLimitOrder(price, volume, balances).then(sendSlackMessage, sendSlackMessage);
+  // Calculate the volume of Bitcoin to buy based on the desired amount and current price
+  let volume = parseFloat(EUR_BUY_AMOUNT / price).toFixed(8);
+
+  // Get the account balances
+  let balances = await getAccountBalances();
+
+  // Place a limit order to buy Bitcoin and send a Slack message with the result
+  try {
+    await placeLimitOrder(price, volume, balances);
+    await sendSlackMessage('Order placed successfully.');
+  } catch (error) {
+    await sendSlackMessage(`Error: ${error}`);
+  }
 }
 
+// Get the current Bitcoin price in Euros
 function getCurrentBitcoinPrice() {
-    return new Promise((resolve, reject) => {
-        kraken.api('Ticker', { "pair": 'BTCEUR' }, function (error, data) {
-            if (error) {
-                reject(error);
-            }
-            else {
-                resolve(data.result['XXBTZEUR']['a'][0] + 20);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    kraken.api('Ticker', { "pair": 'BTCEUR' }, function (error, data) {
+      if (error) {
+        reject(error);
+      } else {
+        // Extract the current price and add 20 as a buffer
+        const price = parseFloat(data.result['XXBTZEUR']['a'][0]);
+        resolve(price + 20);
+      }
     });
+  });
 }
 
+// Get the account balances
+function getAccountBalances() {
+  return new Promise((resolve, reject) => {
+    kraken.api('Balance', null, function (error, data) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data.result);
+      }
+    });
+  });
+}
+
+// Place a limit order to buy Bitcoin
 function placeLimitOrder(price, volume, balances) {
-    return new Promise((resolve, reject) => {
+  let euroBalance = balances['ZEUR'];
 
-        let euroBalance = balances['result']['ZEUR']
+  if (euroBalance < price * volume) {
+    if (euroBalance > 4) {
+      volume = parseFloat(euroBalance / price).toFixed(8);
+    } else {
+      throw new Error(`Insufficient fiat for transaction error. Tried to buy ${volume} BTC for ${price} EUR`);
+    }
+  }
 
-        if (euroBalance < price * volume) {
-            if (euroBalance > 4) {
-                volume = parseFloat(euroBalance / price).toFixed(8);
-            } else {
-                reject('Insufficient fiat for transaction error. Tried to buy ' + volume + ' BTC for' + price + ' EUR');
-            }
-        }
+  const limitOrder = {
+    'pair': 'BTCEUR',
+    'type': 'buy',
+    'ordertype': 'limit',
+    'price': price,
+    'volume': volume
+  };
 
-        var limitOrder = {
-            'pair': 'BTCEUR',
-            'type': 'buy',
-            'ordertype': 'limit',
-            'price': price,
-            'volume': volume
-        };
-
-        kraken.api('AddOrder', limitOrder, function (error, data) {
-            if (error) {
-                reject(error)
-            }
-            else {
-                resolve(data.result);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    kraken.api('AddOrder', limitOrder, function (error, data) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data.result);
+      }
     });
+  });
 }
 
+// Send a Slack message with the provided data
 function sendSlackMessage(data) {
-    return new Promise((resolve, reject) => {
-        axios({
-            method: 'POST',
-            url: slack_web_hook,
-            data: {
-                "type": "mrkdwn",
-                'text': '`' + JSON.stringify(data) + '`'
-            },
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(function (response) {
-                resolve(response);
-            })
-            .catch(function (response) {
-                reject(response);
-            });
-    });
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'POST',
+      url: config.slackWebhook,
+      data: {
+        "type": "mrkdwn",
+        'text': '`' + JSON.stringify(data) + '`'
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(function (response) {
+        resolve(response);
+      })
+      .catch(function (response) {
+        reject(response);
+      });
+  });
 }
--- withdraw.js
+++ withdraw.js
@@ -1,41 +1,76 @@
 const axios = require('axios');
 const KrakenClient = require('kraken-api');
-const key = "INSERT_YOUR_API_KEY";
-const secret = "INSERT_YOUR_API_SECRET";
-const slack_web_hook = "INSERT_YOUR_SLACK_WEBHOOK";
-const wallet_name = "INSERT_YOUR_KRAKEN_WALLET_NAME";
-const kraken = new KrakenClient(key, secret);
 
+// Configuration
+const config = {
+  apiKey: "INSERT_YOUR_API_KEY",
+  apiSecret: "INSERT_YOUR_API_SECRET",
+  slackWebhook: "INSERT_YOUR_SLACK_WEBHOOK",
+  walletName: "INSERT_YOUR_KRAKEN_WALLET_NAME"
+};
+
+const kraken = new KrakenClient(config.apiKey, config.apiSecret);
+
+// Entry point
 main();
 
+// Main function
 async function main() {
-    let balances = await kraken.api('Balance');
-    let btc_balance = parseFloat(balances.result.XXBT);
-    if(btc_balance > 0.002){
-        await kraken.api('Withdraw', { asset : 'XBT', key: wallet_name, amount: btc_balance }).then(sendSlackMessage, sendSlackMessage);
-    }else{
-        sendSlackMessage("No withdrawal initiated. insufficient balance")
+  // Get the account balances
+  let balances = await getAccountBalances();
+
+  // Get the Bitcoin balance
+  let btcBalance = parseFloat(balances.result.XXBT);
+
+  // Check if the balance is above the threshold for withdrawal
+  if (btcBalance > 0.002) {
+    // Initiate the Bitcoin withdrawal to the specified wallet
+    try {
+      await withdrawBitcoin(btcBalance);
+      await sendSlackMessage('Withdrawal initiated successfully.');
+    } catch (error) {
+      await sendSlackMessage(`Error: ${error}`);
     }
+  } else {
+    await sendSlackMessage('No withdrawal initiated. Insufficient balance.');
+  }
 }
 
+// Get the account balances
+function getAccountBalances() {
+  return kraken.api('Balance');
+}
+
+// Initiate a Bitcoin withdrawal
+function withdrawBitcoin(amount) {
+  const withdrawParams = {
+    asset: 'XBT',
+    key: config.walletName,
+    amount: amount
+  };
+
+  return kraken.api('Withdraw', withdrawParams);
+}
+
+// Send a Slack message with the provided data
 function sendSlackMessage(data) {
-    return new Promise((resolve, reject) => {
-        axios({
-            method: 'POST',
-            url: slack_web_hook,
-            data: {
-                "type": "mrkdwn",
-                'text': '`' + JSON.stringify(data) + '`'
-            },
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(function (response) {
-                resolve(response);
-            })
-            .catch(function (response) {
-                reject(response);
-            });
-    });
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'POST',
+      url: config.slackWebhook,
+      data: {
+        "type": "mrkdwn",
+        'text': '`' + JSON.stringify(data) + '`'
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(function (response) {
+        resolve(response);
+      })
+      .catch(function (response) {
+        reject(response);
+      });
+  });
 }
